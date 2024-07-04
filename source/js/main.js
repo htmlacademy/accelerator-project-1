34ef29ac9c7d8e129c586e482c1ae79a41634fd6
restby <restby@gmail.com>
2024-07-04 12:57:47 +0300

    end dev price

--- source/js/main.js
+++ source/js/main.js
@@ -2,3 +2,14 @@
 // import Swiper from "swiper";
 // import {Navigation, Pagination} from "swiper/modules";
 // import 'swiper/css';
+import {initTabs} from "../js/modules/init-tabs";
+import {setActiveTab} from "../js/modules/setActiveTab";
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  window.addEventListener('load', () => {
+
+    initTabs();
+    setActiveTab();
+  });
+});
