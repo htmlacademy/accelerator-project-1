ed7401c8ded5077d33dcf2c41893f627d4f17d38
restby <restby@gmail.com>
2024-07-04 13:23:18 +0300

    some fix price

--- source/js/modules/setActiveTab.js
+++ source/js/modules/setActiveTab.js
@@ -1,44 +1,61 @@
 const setActiveTab = () => {
   if (
     document.querySelector('[data-tabs="element"]') &&
-    document.querySelector(".price-card") &&
-    document.querySelector(".price-card__btn")
+    document.querySelector('.price-card') &&
+    document.querySelector('.price-card__btn')
   ) {
     // Получаем все элементы с классом .price-card
-    const tabs = document.querySelectorAll(".price-card");
+    const tabs = document.querySelectorAll('.price-card');
 
     // Добавляем обработчик события для каждого элемента
     tabs.forEach((tab) => {
-      tab.addEventListener("mouseenter", () => {
+      // Добавляем обработчик события focus для кнопки .price-card__btn
+      const btn = tab.querySelector('.price-card__btn');
+      btn.addEventListener('focus', () => {
+        tab.classList.add('is-active');
+      });
+
+      // Добавляем обработчик события blur для кнопки .price-card__btn
+      btn.addEventListener('blur', () => {
+        tab.classList.remove('is-active');
+      });
+
+      tab.addEventListener('mouseenter', () => {
         // Проверяем наличием класса .is-active у элемента
-        if (!tab.classList.contains("is-active")) {
-          tab.classList.add("is-active"); // Добавляем класс .is-active при наведении
+        if (
+          !tab.classList.contains('is-active') &&
+          !btn.classList.contains('is-active')
+        ) {
+          tab.classList.add('is-active'); // Добавляем класс .is-active при наведении
+          btn.classList.add('is-active'); // Добавляем класс .is-active при наведении
         }
       });
 
-      tab.addEventListener("mouseleave", () => {
+      tab.addEventListener('mouseleave', () => {
         // Проверяем наличием класса .is-active у элемента
-        if (tab.classList.contains("is-active")) {
-          tab.classList.remove("is-active"); // Удаляем класс .is-active при уходе курсора
+        if (tab.classList.contains('is-active') &&
+          btn.classList.contains('is-active')) {
+          tab.classList.remove('is-active'); // Удаляем класс .is-active при уходе курсора
+          btn.classList.remove('is-active'); // Удаляем класс .is-active при уходе курсора
         }
       });
     });
 
     // Получаем все элементы .price-card
-    const priceCards = document.querySelectorAll(".price-card");
+    // const tabs = document.querySelectorAll(".price-card");
 
     // Добавляем обработчик события focus для кнопки .price-card__btn
-    priceCards.forEach((priceCard) => {
-      const btn = priceCard.querySelector(".price-card__btn");
-      btn.addEventListener("focus", () => {
-        priceCard.classList.add("is-active");
-      });
+    // tabs.forEach((tab) => {
+    //   const btn = tab.querySelector(".price-card__btn");
+    //   btn.addEventListener("focus", () => {
+    //     tab.classList.add('is-active');
+    //   });
 
-      // Добавляем обработчик события blur для кнопки .price-card__btn
-      btn.addEventListener("blur", () => {
-        priceCard.classList.remove("is-active");
-      });
-    });
+    //   // Добавляем обработчик события blur для кнопки .price-card__btn
+    //   btn.addEventListener("blur", () => {
+    //     tab.classList.remove('is-active');
+    //   });
+    // });
   }
 };
 
