4a017392b4d3411519d364b42eb33ee33d5e183f
Давлетшина Родика <47739600+Rodika94@users.noreply.github.com>
2024-07-14 03:17:49 +0500

    swipper

--- source/js/main.js
+++ source/js/main.js
@@ -1,4 +1,23 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
+import Swiper from "swiper";
+import {Navigation} from "swiper/modules";
+
+document.documentElement.classList.remove('no-js');
+
+new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 40,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1366: {
+      slidesPerView: 4,
+    }
+  },
+  modules: [ Navigation ],
+  navigation: {
+    nextEl: '.juri-btn--right',
+    prevEl: '.juri-btn--left',
+  },
+  loop: true,
+});
